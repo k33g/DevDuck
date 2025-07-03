b4906b6d31e17a37bffc91b2202c6b5d243b5c7d
k33g <ph.charriere@gmail.com>
2025-07-03 16:29:12 +0200

    🛟 Updated.

--- agents/devduck/agent.py
+++ agents/devduck/agent.py
@@ -2,12 +2,14 @@ import os
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 
-os.environ["OPENAI_API_KEY"] = "tada"
-os.environ["OPENAI_API_BASE"] = f"{os.environ.get('DMR_BASE_URL')}/engines/llama.cpp/v1"
-
 root_agent = Agent(
-    model=LiteLlm(model=f"openai/{os.environ.get('MODEL_RUNNER_CHAT_MODEL')}"),
+    model=LiteLlm(
+        model=f"openai/{os.environ.get('MODEL_RUNNER_CHAT_MODEL')}",
+        api_base=os.environ.get("MODEL_RUNNER_BASE_URL"),
+        api_key="tada",
+    ),
     name=f"{os.environ.get('AGENT_NAME')}",
     description=f"{os.environ.get('AGENT_DESCRIPTION')}",
     instruction=f"{os.environ.get('AGENT_INSTRUCTION')}",
 )
+
